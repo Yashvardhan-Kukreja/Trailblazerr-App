729929fec553b9b0a29025034b1b734490ce8f55
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-07-08 02:17:05 +0530

    Fixed the 'addOrUpdateSession' functionality

--- app/src/main/java/com/yash1300/trailblazerr/Utils/RealmController.java
+++ app/src/main/java/com/yash1300/trailblazerr/Utils/RealmController.java
@@ -1,7 +1,7 @@
 package com.yash1300.trailblazerr.Utils;
 
+
 import com.yash1300.trailblazerr.Models.DaimokuSession;
-import com.yash1300.trailblazerr.Models.Date;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -20,7 +20,6 @@ public class RealmController {
             realmInstance = new RealmController();
         return realmInstance;
     }
-/*
 
     public RealmResults<DaimokuSession> getHistory() {
         return realm.where(DaimokuSession.class).findAll();
@@ -35,6 +34,28 @@ public class RealmController {
     public boolean isEmptyHistory() {
         return realm.allObjects(DaimokuSession.class).isEmpty();
     }
-*/
+
+    public DaimokuSession findSessionByDate(String date) {
+        return realm.where(DaimokuSession.class).equalTo("date", date).findFirst();
+    }
+
+    public void addOrUpdateASession(String date, long secs) {
+        RealmResults<DaimokuSession> results = this.getHistory();
+        long todaySecs = 0;
+        if (this.findSessionByDate(date) != null) {
+            todaySecs = results.get(results.size()-1).getDuration_in_secs();
+        }
+        realm.beginTransaction();
+        realm.copyToRealmOrUpdate(new DaimokuSession(date, todaySecs+secs));
+        realm.commitTransaction();
+    }
+
+    public DaimokuSession yesterdaySession(String todayDate) {
+        RealmResults<DaimokuSession> results = this.getHistory();
+        if (this.findSessionByDate(todayDate) != null) {
+            return results.get(results.size()-2);
+        }
+        return results.get(results.size()-1);
+    }
 
 }
